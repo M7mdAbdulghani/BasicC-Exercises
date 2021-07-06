29e09f4c8db89afe04213c9dd26bcf0908506d54
m-abdulghani <m.abdulghani@bc.com.sa>
2021-07-06 20:14:28 +0300

    Exercise 2

--- BasicAlgorithmsCSharp150/Program.cs
+++ BasicAlgorithmsCSharp150/Program.cs
@@ -12,8 +12,15 @@ namespace BasicAlgorithmsCSharp150
                 Write a C# Sharp program to compute the sum of the two given integer values. 
                 If the two values are the same, then return triple their sum.
             */
-            SolveExerciseOne();
-            
+            // SolveExerciseOne();
+
+            /*
+                Exercise 2
+                ***************************************************************************************** 
+                Write a C# Sharp program to get the absolute difference between n and 51. 
+                If n is greater than 51 return triple the absolute difference
+            */
+            // SolveExerciseTwo();
         }
         public static void SolveExerciseOne()
         {
@@ -35,6 +42,22 @@ namespace BasicAlgorithmsCSharp150
 
             result = (number1 == number2) ? (number1 * 6) : (number1 + number2);
             Console.WriteLine("Result is {0}", result);
+        }
+
+        public static void SolveExerciseTwo()
+        {
+            const int number = 51;
+            int userNumber;
+
+            Console.WriteLine("Enter a number");
+
+            while(!int.TryParse(Console.ReadLine(), out userNumber))
+            {
+                Console.WriteLine("Invalid Input, try again");
+            }
+
+            int result = (number > userNumber) ? (number - userNumber) : (userNumber - number) * 3;
+            Console.WriteLine("The difference is {0}", result);
         }
     }
 }
